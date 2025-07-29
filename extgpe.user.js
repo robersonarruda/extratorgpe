2e05b99d5016b7e1d75957c36126c85cb7399a9f
robersonarruda <99708420+robersonarruda@users.noreply.github.com>
2025-07-29 12:48:39 -0400

    Update extgpe.user.js

--- extgpe.user.js
+++ extgpe.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Extrator Contatos GPE 3
 // @fullname      Extrator Contatos GPE 3
-// @version    3.3.0.1
+// @version    3.3.5.1
 // @description  Consulta e salva dados de contato dos Servidores do sigeduca.
 // @include	*sigeduca.seduc.mt.gov.br/grh/hwmgrhServidor.aspx*
 // @author       Roberson Arruda
@@ -27,13 +27,8 @@ styleSCT.innerHTML =
 '	-moz-box-shadow:inset 1px 1px 0px 0px #b2ced4;'+
 '	-webkit-box-shadow:inset 1px 1px 0px 0px #b2ced4;'+
 '	box-shadow:inset 1px 1px 0px 0px #b2ced4;'+
-'	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #4e88ed), color-stop(1, #3255c7) );'+
-'	background:-moz-linear-gradient( center top, #4e88ed 5%, #3255c7 100% );'+
 '	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#4e88ed", endColorstr="#3255c7");'+
 '	background-color:#4e88ed;'+
-'	-moz-border-radius:4px;'+
-'	-webkit-border-radius:4px;'+
-'	border-radius:4px;'+
 '	border:1px solid #102b4d;'+
 '	display:inline-block;'+
 '	color:#ffffff;'+
@@ -56,7 +51,8 @@ styleSCT.innerHTML =
 '	-moz-border-radius:4px;'+
 '	-webkit-border-radius:4px;'+
 '	border-radius:4px;'+
-'	border:1px solid #102b4d;}'
+'	border:1px solid #b9b8b9;'+
+'   background:radial-gradient(#c8ced5, #d5d5d533)}'
 document.getElementsByTagName('head')[0].appendChild(styleSCT);
 
 //Variáveis
@@ -64,7 +60,7 @@ var nomeArquivo;
 var vetServidor = [0];
 var dadosPessoal = [0];
 var dadosCTPS = [0];
-var k = 0;
+var codUnidade="";
 var n = 0;
 var o = 0;
 var urlDadosIni = "";
@@ -80,52 +76,35 @@ var urlCTPSServIni = "http://sigeduca.seduc.mt.gov.br/grh/hwtgrhpessoa2.aspx?";
 var urlCTPSServFim = ",HWMGrhServidor,DSP,2,";
 var urlProcessos = "http://sigeduca.seduc.mt.gov.br/grh/hwmgrhconsultaprocesso.aspx?es"
 
-//FUNÇÃO SALVAR CONTEÚDO EM TEXTO
+function resetarVariaveis(){
+    vetServidor = [0];
+    dadosPessoal = [0];
+    dadosCTPS = [0];
+    txtareaDados.value ="";
+    n = 0;
+    o = 0;
+}
+
+//FUNÇÃO SALVAR CONTEÚDO EM CSV
 function saveTextAsFile() {
-    var conteudo = document.getElementById("txtDados").value; //Retira acentos =>> .normalize('NFD').replace(/[\u0300-\u036f]/g, "");
+    var conteudo = document.getElementById("txtDados").value;
     var a = document.createElement('a');
-    with (a) {
-        var href='data:text/csv;base64,' + btoa(conteudo);
-        var download= nomeArquivo+'.csv';
-    }
+
+    // Atribuindo diretamente as propriedades do objeto 'a'
+    a.href = 'data:text/csv;base64,' + btoa(conteudo);
+    a.download = nomeArquivo + '.csv';
+
+    // Adicionando o link ao DOM, clicando e removendo
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
 }
 
-function obterCodUnidade(){
-    ifrIframe1.removeEventListener("load", obterCodUnidade);
-    var optUnidade = parent.frames[0].document.getElementById('vWGERLOTCOD');
-    txtareaCodUnidade.value = optUnidade[optUnidade.selectedIndex].value.trim();
-    iniciar();
-}
-
 function iniciar(){
+    resetarVariaveis();
     nomeArquivo = elemSelServ[elemSelServ.selectedIndex].innerText.trim(); //Nome que será dado ao arquivo com base no Tipo de servidor selecionado atual.
-    if(txtareaCodUnidade.value !== ""){
+    if(codUnidade !== ""){
         ifrIframe1.removeEventListener("load", coletaDados);
-        //btnColetaFicha.disabled = true;
-        vetServidor = [0];
-        var zeros="00";
-        var finalizado = false;
-        function copyCodServ(){
-            parent.frames[0].document.getElementById('vGERLOTCOD').value = txtareaCodUnidade.value;
-            parent.frames[0].document.getElementById('vGRHTPOSRVCOD').value = elemSelServ.value;
-            parent.frames[0].document.getElementsByName('BCONSULTAR')[0].click();
-            setTimeout(()=>{
-                var tabela = parent.frames[0].document.getElementById('Grid1ContainerTbl');
-                var linhas = tabela.getElementsByTagName('tr').length-1;
-                if(linhas>=1){
-                    for (var i = 1; i <= linhas; i++){
-                        if(i==10){zeros="0"};
-                        if(i==100){zeros=""};
-                        vetServidor[i-1] = parent.frames[0].document.getElementById('span_GERPESCOD_0'+zeros+i).innerText.trim();
-                        if(i==linhas){vetServidor = [...new Set(vetServidor)];ifrIframe1.removeEventListener("load", copyCodServ); coletar()};
-                    }
-                }
-                else{alert("nenhum servidor localizado para o Tipo Selecionado")}
-            },2000);
-        }
         ifrIframe1.src = urlListaServ;
         ifrIframe1.addEventListener("load", copyCodServ);
     }
@@ -135,22 +114,68 @@ function iniciar(){
     }
 }
 
-function coletar()
-{
-    n=0;
-    o=0;
-    k=vetServidor.length;
-    while(k >= 0){
-        if(vetServidor[k]==""){vetServidor.splice(k, 1)}
-            k--;
-        if(k < 0){
-            dadosPessoal = [0];
-            dadosCTPS = [0];
-            txtareaDados.value ="";
-            ifrIframe1.src= urlDadosIni+vetServidor[n]+urlDadosFim;
-            ifrIframe1.addEventListener("load", coletaDados);
+function obterCodUnidade(){
+    ifrIframe1.removeEventListener("load", obterCodUnidade);
+    var optUnidade = parent.frames[0].document.getElementById('vWGERLOTCOD');
+    codUnidade = optUnidade[optUnidade.selectedIndex].value.trim();
+    iniciar();
+}
+
+function copyCodServ(){
+    // Função que será chamada quando o 'gx_ajax_notification' desaparecer
+    function onElementHidden() {
+        var tabela = parent.frames[0].document.getElementById('Grid1ContainerTbl');
+        var nLinhas = tabela.getElementsByTagName('tr').length - 1;
+        if (nLinhas >= 1) {
+            for (var i = 1; i <= nLinhas; i++) {
+                vetServidor[i-1] = parent.frames[0].document.getElementById('span_GERPESCOD_'+String(i).padStart(4, '0')).innerText.trim();
+                if (i == nLinhas) {
+                    vetServidor = [...new Set(vetServidor)];
+                    ifrIframe1.removeEventListener("load", copyCodServ);
+                    vetServidor = vetServidor.filter(function(item) {return item !== ""; });
+                    ifrIframe1.src= urlDadosIni+vetServidor[n]+urlDadosFim;
+                    ifrIframe1.addEventListener("load", coletaDados);
+                }
+            }
+        } else {
+            alert("Nenhum servidor localizado para o Tipo Selecionado ou sistema indisponível no momento. Tente novamente mais tarde!");
         }
     }
+
+    // Configuração do MutationObserver para observar alterações no estilo de 'gx_ajax_notification'
+    function observeNotification() {
+        var targetNode = parent.frames[0].document.getElementById('gx_ajax_notification');
+        if (!targetNode) return; // Se o elemento não existe, não faz nada.
+
+        var config = { attributes: true, attributeFilter: ['style'] };
+
+        // Callback para o MutationObserver
+        var callback = function (mutationsList, observer) {
+            mutationsList.forEach(function (mutation) {
+                if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+                    // Verifica se 'display' é 'none'
+                    var displayValue = targetNode.style.display;
+                    if (displayValue === 'none') {
+                        // Quando o elemento desaparecer, chama a função desejada
+                        onElementHidden();
+                        observer.disconnect(); // Desconecta o observer após a execução
+                    }
+                }
+            });
+        };
+
+        // Cria e começa a observar o 'gx_ajax_notification'
+        var observer = new MutationObserver(callback);
+        observer.observe(targetNode, config);
+    }
+    // Inicia a observação. Quando o gx_ajax_notification for exibido e sumir, significa que a lista de servidores consultada logo abaixo já foi exibida, chamando a função onElementHidden.
+    observeNotification();
+
+    //Consulta lista de servidores da categoria selecionada, para pegar os códigos deles.
+    parent.frames[0].document.getElementById('vGERLOTCOD').value = codUnidade;
+    parent.frames[0].document.getElementById('vGRHTPOSRVCOD').value = elemSelServ.value;
+    parent.frames[0].document.getElementsByName('BCONSULTAR')[0].click();
+
 }
 
 function coletaDados() {
@@ -187,7 +212,7 @@ function coletaDados() {
         dadosPessoal[n] = dadosPessoal[n] + parent.frames[0].document.getElementById('span_CTLGERPESENDCIDDSC').innerHTML+";"; // cidade
         dadosPessoal[n] = dadosPessoal[n] + parent.frames[0].document.getElementById('span_CTLGERPESENDUF').innerHTML+";"; // uf
         dadosPessoal[n] = dadosPessoal[n] + parent.frames[0].document.getElementById('CTLGERPESCEP').value+";"; // cep
-        txtareaDados.value = "extraindo informações de "+vetServidor[o];
+        txtareaDados.value = "extraindo informações de "+vetServidor[n];
         n=n+1;
         if(n < vetServidor.length){
             ifrIframe1.src = urlDadosIni+vetServidor[n]+urlDadosFim;
@@ -202,10 +227,9 @@ function coletaDados() {
     if(n >= vetServidor.length){
         //DADOS DO CTPS
         dadosCTPS[o] = vetServidor[o] +";"; //Cod Servidor
-        //a = a + parent.frames[0].document.getElementById('span_vGRHPESNOM').innerText.trim()+";"; //Nome Servidor (desativado pois não carrega sem antes abrir a FICHA)
         dadosCTPS[o] = dadosCTPS[o] + parent.frames[0].document.getElementById('CTLGRHSRVNMRCRTTRB').value +";"; //Nro Carteria Trab:
         dadosCTPS[o] = dadosCTPS[o] + parent.frames[0].document.getElementById('CTLGRHSRVSERCRTTRB').value +";"; //Serie Carteria Trab:
-        var optUFCTPS = parent.frames[0].document.getElementById('CTLGRHSRVUFCRTTRB'); //Elemento option da UF da CTPS
+        let optUFCTPS = parent.frames[0].document.getElementById('CTLGRHSRVUFCRTTRB'); //Elemento option da UF da CTPS
         dadosCTPS[o] = dadosCTPS[o] + optUFCTPS[optUFCTPS.selectedIndex].innerText.trim()+";";//UF da CTPS selecionado
         dadosCTPS[o] = dadosCTPS[o] + parent.frames[0].document.getElementById('CTLGRHSRVNMRTITELE').value +";"; //Nro Título Eleitor:
         dadosCTPS[o] = dadosCTPS[o] + parent.frames[0].document.getElementById('CTLGRHSRVSECTITELE').value +";"; //Seção:
@@ -253,7 +277,7 @@ var divCorpo = document.createElement('div');
     divCorpo.setAttribute('id','credito1');
     divCorpo.setAttribute('name','credito2');
     divCorpo.setAttribute('class','menuSCT');
-    divCorpo.setAttribute('style','background: #DBDBDB; color: #000; width: 300px; text-align: center;font-weight: bold;position: fixed;z-index: 2002;padding: 5px 0px 0px 5px;bottom: 24px;right: 30px;height: 340px;');
+    divCorpo.setAttribute('style',' color: #000; width: 300px; text-align: center;font-weight: bold;position: fixed;z-index: 2002;padding: 5px 0px 0px 5px;bottom: 24px;right: 30px;height: 340px;');
 document.getElementsByTagName('body')[0].appendChild(divCorpo);
 
 //Iframe
@@ -307,22 +331,6 @@ var comboServ = document.getElementById("selServ");
 
 br1 = document.createElement("br");//Quebra de linha
 divCorpo.appendChild(br1); //Quebra de linha
-
-//TEXTO CÓDIGO DA UNIDADE
-//var textCodUnidade = document.createTextNode("CÓDIGO DA ESCOLA");
-//divCorpo.appendChild(textCodUnidade);
-
-br1 = document.createElement("br");//Quebra de linha
-divCorpo.appendChild(br1); //Quebra de linha
-
-//textarea codigo da unidade
-var txtareaCodUnidade = document.createElement('TEXTAREA');
-txtareaCodUnidade.setAttribute('name','txtCodUnidade');
-txtareaCodUnidade.setAttribute('id','txtCodUnidade');
-txtareaCodUnidade.setAttribute('value','10421');
-txtareaCodUnidade.setAttribute('style','border:1px solid #000000;width: 100px;height: 22px; resize: none; overflow-y: hidden; display: none');
-txtareaCodUnidade.disabled;
-divCorpo.appendChild(txtareaCodUnidade);
 
 br1 = document.createElement("br");//Quebra de linha
 divCorpo.appendChild(br1); //Quebra de linha
